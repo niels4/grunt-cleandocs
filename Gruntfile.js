d07bc58cc84e453b8aa2fc7e2d096fdf24f0f506
Niels Nielsen <niels@protectwise.com>
2014-02-16 21:55:33 -0700

    cleaned up gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,22 +30,9 @@ module.exports = function(grunt) {
 
     // Configuration to be run (and then tested).
     cleandocs: {
-      default_options: {
-        options: {
-        },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!',
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
-      },
+      all: {
+        options: {}
+      }
     },
 
     // Unit tests.
