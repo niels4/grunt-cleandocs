1ab1814eaa88d17af41e443654e1e990f96d5243
Niels Nielsen <niels@protectwise.com>
2014-02-16 21:49:54 -0700

    fixed the cleandocs dependency

--- tasks/cleandocs.js
+++ tasks/cleandocs.js
@@ -8,21 +8,22 @@
 'use strict';
 var cleandocs = require('cleandocs');
 
+var defaultOptions = {
+  "docSuffix": ".md",
+  "docTagStart": "#*c:",
+  "docTagEnd": "*",
+  "srcSuffix": ".coffee",
+  "srcTagStart": "#*c:",
+  "srcTagEnd": "*",
+  "outputSuffix": ".litcoffee",
+  "defaultTagOrder": ["untagged", "description"]
+};
+
 module.exports = function(grunt) {
 
   grunt.registerMultiTask('cleandocs', 'Merge seperate documentation and source files to create literate coffee style documentation.', function() {
-    var options = this.options({
-      "docSuffix": ".md",
-      "docTagStart": "#*c:",
-      "docTagEnd": "*",
-      "srcSuffix": ".coffee",
-      "srcTagStart": "#*c:",
-      "srcTagEnd": "*",
-      "outputSuffix": ".litcoffee",
-      "defaultTagOrder": ["untagged", "description"]
-    });
+    var options = this.options(defaultOptions);
 
-    console.log("cleandocs options", options);
     cleandocs.processAllDirs(options);
   });
 
